16b9dfd9c46fb6b8f8a571b6e3b10b126b1bb402
Javier Rotelli <javier.rotelli@gmail.com>
2013-09-25 22:09:12 -0300

    puse el avion, con la camara que lo sigue a donde va

--- MiGrupo/EjemploAlumno.cs
+++ MiGrupo/EjemploAlumno.cs
@@ -8,6 +8,9 @@ using System.Drawing;
 using Microsoft.DirectX;
 using TgcViewer.Utils.Modifiers;
 using TgcViewer.Utils.Terrain;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.Input;
+using Microsoft.DirectX.DirectInput;
 
 namespace AlumnoEjemplos.MiGrupo
 {
@@ -17,7 +20,8 @@ namespace AlumnoEjemplos.MiGrupo
     public class EjemploAlumno : TgcExample
     {
         private TgcSkyBox skyBox;
-        
+        private TgcMesh avion;
+        float velocidadActual = 200f;
         /// <summary>
         /// Categoría a la que pertenece el ejemplo.
         /// Influye en donde se va a haber en el árbol de la derecha de la pantalla.
@@ -53,24 +57,24 @@ namespace AlumnoEjemplos.MiGrupo
             //GuiController.Instance: acceso principal a todas las herramientas del Framework
 
             //Device de DirectX para crear primitivas
-            Device d3dDevice = GuiController.Instance.D3dDevice;
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
 
             //Carpeta de archivos Media del alumno
             string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
 
 
             ///////////////USER VARS//////////////////
-
+            /*
             //Crear una UserVar
             GuiController.Instance.UserVars.addVar("variablePrueba");
 
             //Cargar valor en UserVar
             GuiController.Instance.UserVars.setValue("variablePrueba", 5451);
-
+            */
 
 
             ///////////////MODIFIERS//////////////////
-
+            /*
             //Crear un modifier para un valor FLOAT
             GuiController.Instance.Modifiers.addFloat("valorFloat", -50f, 200f, 0f);
 
@@ -81,52 +85,36 @@ namespace AlumnoEjemplos.MiGrupo
             //Crear un modifier para modificar un vértice
             GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
 
-
-
-            ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
-            //Es la camara que viene por default, asi que no hace falta hacerlo siempre
-            GuiController.Instance.RotCamera.Enable = true;
-            //Configurar centro al que se mira y distancia desde la que se mira
-            GuiController.Instance.RotCamera.setCamera(new Vector3(0, 0, 0), 100);
-
-
-            /*
-            ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
-            //Camara en primera persona, tipo videojuego FPS
-            //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
-            //Por default la camara FPS viene desactivada
-            GuiController.Instance.FpsCamera.Enable = true;
-            //Configurar posicion y hacia donde se mira
-            GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
             */
 
+            /*
+             * Configuracion del avion
+             */
+            TgcSceneLoader loader = new TgcSceneLoader();
+            TgcScene scene = loader.loadSceneFromFile(
+                GuiController.Instance.ExamplesMediaDir + "MeshCreator\\Meshes\\Vehiculos\\AvionCaza\\AvionCaza-TgcScene.xml");
+            avion = scene.Meshes[0];
+            avion.rotateY((float)Math.PI);  //el modelo aparece invertido sino
 
             /*
-            ///////////////LISTAS EN C#//////////////////
-            //crear
-            List<string> lista = new List<string>();
+             * Copnfiguracion de la camara
+             */
+            GuiController.Instance.ThirdPersonCamera.Enable = true;
+            GuiController.Instance.ThirdPersonCamera.setCamera(avion.Position, 100, -400);
+            GuiController.Instance.ThirdPersonCamera.TargetDisplacement = new Vector3(0, 100, 0);
+            
+            /*
+             * Heightmap
+             */
+            currentTexture = GuiController.Instance.ExamplesMediaDir + "Heighmaps\\" + "TerrainTexture2.jpg";
+            //Path de Heightmap default del terreno y Modifier para cambiarla
+            currentHeightmap = GuiController.Instance.ExamplesMediaDir + "Heighmaps\\" + "Heightmap2.jpg";
+            //Cargar terreno: cargar heightmap y textura de color
+            terrain = new TgcSimpleTerrain();
+            terrain.loadHeightmap(currentHeightmap, 20f, 1.3f, new Vector3(0, 0, 300));
+            terrain.loadTexture(currentTexture);
 
-            //agregar elementos
-            lista.Add("elemento1");
-            lista.Add("elemento2");
-
-            //obtener elementos
-            string elemento1 = lista[0];
-
-            //bucle foreach
-            foreach (string elemento in lista)
-            {
-                //Loggear por consola del Framework
-                GuiController.Instance.Logger.log(elemento);
-            }
-
-            //bucle for
-            for (int i = 0; i < lista.Count; i++)
-            {
-                string element = lista[i];
-            }*/
-
-            string texturesPath = GuiController.Instance.ExamplesMediaDir + "Texturas\\Quake\\SkyBox1\\";
+            string texturesPath = GuiController.Instance.ExamplesMediaDir + "Texturas\\Quake\\SkyBox LostAtSeaDay\\";
 
             //Crear SkyBox 
             skyBox = new TgcSkyBox();
@@ -137,14 +125,14 @@ namespace AlumnoEjemplos.MiGrupo
             //skyBox.Color = Color.OrangeRed;
 
             //Configurar las texturas para cada una de las 6 caras
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Up, texturesPath + "phobos_up.jpg");
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Down, texturesPath + "phobos_dn.jpg");
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Left, texturesPath + "phobos_lf.jpg");
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Right, texturesPath + "phobos_rt.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Up, texturesPath + "lostatseaday_up.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Down, texturesPath + "lostatseaday_dn.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Left, texturesPath + "lostatseaday_lf.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Right, texturesPath + "lostatseaday_rt.jpg");
 
             //Hay veces es necesario invertir las texturas Front y Back si se pasa de un sistema RightHanded a uno LeftHanded
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Front, texturesPath + "phobos_bk.jpg");
-            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Back, texturesPath + "phobos_ft.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Front, texturesPath + "lostatseaday_bk.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Back, texturesPath + "lostatseaday_ft.jpg");
 
 
 
@@ -163,35 +151,64 @@ namespace AlumnoEjemplos.MiGrupo
         public override void render(float elapsedTime)
         {
             //Device de DirectX para renderizar
-            Device d3dDevice = GuiController.Instance.D3dDevice;
-
-
-            //Obtener valor de UserVar (hay que castear)
-            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
-
-
-            //Obtener valores de Modifiers
-            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
-            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
-            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
-
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
+            TgcD3dInput d3dInput = GuiController.Instance.D3dInput;
+            //Multiplicar la velocidad por el tiempo transcurrido, para no acoplarse al CPU
+            float speed = velocidadActual * elapsedTime;
 
             ///////////////INPUT//////////////////
             //conviene deshabilitar ambas camaras para que no haya interferencia
-
-            //Capturar Input teclado 
-            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            //Calcular proxima posicion del avion segun Input
+            Vector3 move = new Vector3(0, 0, 0);
+            bool moving = false;
+            //Adelante
+            if (d3dInput.keyDown(Key.W))
             {
-                //Tecla F apretada
+                move.Z = speed;
+                avion.Rotation = new Vector3(0, (float)Math.PI, 0);
+                moving = true;
+            }//Atras
+            else if (d3dInput.keyDown(Key.S))
+            {
+                move.Z = -speed;
+                avion.Rotation = new Vector3(0, 0, 0);
+                moving = true;
+            }//Izquierda
+            else if (d3dInput.keyDown(Key.A))
+            {
+                move.X = -speed;
+                avion.Rotation = new Vector3(0, (float)Math.PI / 2, 0);
+                moving = true;
+            }//Derecha
+            else if (d3dInput.keyDown(Key.D))
+            {
+                move.X = speed;
+                avion.Rotation = new Vector3(0, -(float)Math.PI / 2, 0);
+                moving = true;
             }
 
-            //Capturar Input Mouse
-            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            //Si hubo desplazamientos
+            if (moving)
             {
-                //Boton izq apretado
+                //Mover personaje
+                Vector3 lastPos = avion.Position;
+                avion.move(move);
             }
 
-            skyBox.render();
+            //Renderizar modelo
+            avion.render();
+
+            //Renderizar BoundingBox
+            avion.BoundingBox.render();
+
+            terrain.render();
+            
+
+            //Hacer que la camara siga al personaje en su nueva posicion
+            GuiController.Instance.ThirdPersonCamera.Target = avion.Position;
+
+
+            //skyBox.render();
         }
 
         /// <summary>
@@ -203,5 +220,11 @@ namespace AlumnoEjemplos.MiGrupo
 
         }
 
+
+        public string currentHeightmap { get; set; }
+
+        public TgcSimpleTerrain terrain { get; set; }
+
+        public string currentTexture { get; set; }
     }
 }
